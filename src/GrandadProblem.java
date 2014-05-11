b362ea81b0ff37627c0dd921196ba72559a7f01e
ThisIsMyPlace <thisismyplace@hushmail.me>
2014-05-11 19:10:54 +0100

    What the fuck was that?

--- src/GrandadProblem.java
+++ src/GrandadProblem.java
@@ -5,17 +5,14 @@ public class GrandadProblem {
 		int testValue = 0;
 		int leftOverBars[] = new int[] { 6, 2, 1, 0 };
 		
-		while(true) {
-			for(int noOfThieves = 7; noOfThieves > 3; noOfThieves--) {
-				int leftOverCount = leftOverBars[7 - noOfThieves];
-				if(testValue % noOfThieves == leftOverCount) {
-					
-				} else {
-					testValue++;
-					noOfThieves = 8;
-				}
+		for(int noOfThieves = 7; noOfThieves > 3; noOfThieves--) {
+			int leftOverCount = leftOverBars[7 - noOfThieves];
+			if(testValue % noOfThieves == leftOverCount) {
+				
+			} else {
+				testValue++;
+				noOfThieves = 8;
 			}
-			break;
 		}
 		System.out.println(testValue);
 	}
